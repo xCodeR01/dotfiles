44ec1b48308df55f420033c1c57da3f39b4042f6
Raven Redstain <ravenredstain@outlook.com>
2022-06-04 22:58:54 +0500

    fixed problem with lsp

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,18 +1,9 @@
 local keymap = vim.keymap.set
-local term_opts = { silent = true }
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
 
 -- Normal --
 -- Better window navigation
